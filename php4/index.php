19862cbe67991ca5ddc5a7071d486ee75c8cb94d
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-04-28 21:30:22 +0600

    phpProject

---
+++ php4/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php syntax</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption{
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>PHP Fundamentals Training</h2>
+    </div>
+
+    <section class="maincontent">
+    <?php
+    echo "PHP Fundamentals Bangla Tutorial Part-03 (PHP Syntax)";
+    ?>
+    </section>
+
+
+ <div class="footeroption">
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+ 
+  
+</body>
+</html>
