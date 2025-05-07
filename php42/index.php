72e6fe3fee5aa0ba6802b944562570c4d2d8d058
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-07 17:21:09 +0600

    php42

---
+++ php42/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Fundamentals Bangla Tutorial Part-41 (Error Reporting)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption{
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+        PHP Fundamentals Bangla Tutorial Part-41 (Error Reporting)</h2>
+    </div>
+
+   
+
+    <section class="maincontent">
+    <?php
+    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    $price = 45;
+    if($price == 45){
+        print "The price is $price";
+    } else {
+        print "Price is not 45";
+    }
+      
+    ?>
+    </section>
+
+
+ <div class="footeroption">
+ <p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+</body>
+</html>
