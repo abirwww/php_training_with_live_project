809f28d2f3713c358ea61dad8e52e163112010a5
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-07 15:52:08 +0600

    php40

---
+++ php40/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Fundamentals Bangla Tutorial Part-39 (Filters)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption{
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+        PHP Fundamentals Bangla Tutorial Part-39 (Filters)</h2>
+    </div>
+
+   
+
+    <section class="maincontent">
+    <?php
+    
+        $url = "www.usa.com";
+        if(filter_var($url, FILTER_VALIDATE_URL)){
+            echo "$url is valid Website Address";
+        } else {
+            echo "$url is not valid website address";
+        }
+
+
+
+    ?>
+    </section>
+
+
+ <div class="footeroption">
+ <p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+</body>
+</html>
