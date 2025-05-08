196ba7526cba88003398a639206eb333677ac651
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-08 15:07:40 +0600

    php46

--- php46/index.php
+++ php46/index.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Fundamentals Bangla Tutorial Part-44 (Case Change)</title>
+    <title>PHP Fundamentals Bangla Tutorial Part-45 (Strip whiltespace/Characters)</title>
     <style>
         .phpcoding{
             width: 900px;
             margin: 0 auto;
             background: #ddd;
         }
-        .headeroption, .footeroption{
+        .headeroption, .footeroption
+        
+        {
             background:#444;
             color:#fff;
             text-align: center;
@@ -25,41 +27,20 @@
 <section class="phpcoding">
     <div class="headeroption">
         <h2>
-        PHP Fundamentals Bangla Tutorial Part-44 (Case Change)</h2>
+        PHP Fundamentals Bangla Tutorial Part-45 (Strip whiltespace/Characters)</h2>
     </div>
 
    
 
     <section class="maincontent">
     <?php
-
-
-        // if(isset($_POST['text'])){
-        //     $text = $_POST['text'];
-        //     echo $text;
-        // }
-
-        // if(isset($_POST['text'])){
-        //     $text = $_POST['text'];
-        //     $words = explode(" ", $text);
-        //     echo "Total words: ".count($words);
-        // }
-
-        // if(isset($_POST['text'])){
-        //     $text = $_POST['text'];
-        //     $words = explode(" ", $text);
-        //     $reversed = array_reverse($words);
-        //     echo implode(" ", $reversed);
-        // }
-
-        if(isset($_POST['text'])){
-            $txt = $_POST['text'];
-            // echo strtoupper($txt);
-            echo ucfirst($txt);
-        }
-
+    
+    if(isset($_POST['text'])){
+        global $text;
+        $txt = $_POST['text'];
+        echo trim($txt, " .");
+    }
     ?>
-
     <form action="index.php" method="post">
         <input type="text" name="text" />
         <input type="submit" value="Submit" />
