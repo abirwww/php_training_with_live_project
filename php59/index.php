524480046997896b4a38da528a24c090c9246d9a
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-12 11:18:10 +0600

    php59

---
+++ php59/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+    
+PHP Array Functions Bangla Tutorial Part-11 (array_intersect_key)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption{
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+        
+PHP Array Functions Bangla Tutorial Part-11 (array_intersect_key)</h2>
+    </div>
+
+    <section class="maincontent">
+    <?php
+   
+        $arr = array(
+            "a" => "red",
+            "b" => "green",
+            "c" => "blue",
+            "d" => "yelloow"
+        );
+
+        if(array_key_exists("a", $arr)){
+            echo "Key already exists.";
+        } else {
+            echo "Key does not exist";
+        }
+
+        // $array_two = array(
+        //     "a" => "red",
+        //     "f" => "green",
+        //     "g" => "blue"
+        // );
+
+       
+
+        //$result = array_key_exists($array_one, $array_two);
+
+        // print ("<pre>");
+        // print_r($result);
+        // print("</pre>");
+   
+    ?>
+    </section>
+
+
+ <div class="footeroption">
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+ 
+  
+</body>
+</html>
