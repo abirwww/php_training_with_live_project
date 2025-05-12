67a92fe25fc3d425bb53668b89db64d46c16c0e2
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-12 15:01:22 +0600

    php64

---
+++ php64/index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Array Functions Bangla Tutorial Part-17 (array_pad)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption
+        
+        {
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+      PHP Array Functions Bangla Tutorial Part-17 (array_pad)</h2>
+    </div>
+
+   
+
+    <section class="maincontent">
+    <?php
+    
+        $arr = array("red", "green");
+        $result = array_pad($arr, 8, "blue");
+        
+        
+    print("<pre>");
+    print_r($result);
+    print("</pre>");
+
+  
+    ?>
+  
+    </section>
+
+
+ <div class="footeroption">
+ <p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+</body>
+</html>
