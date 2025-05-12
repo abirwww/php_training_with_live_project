2901ef1ae077f5f0f3f646d944d54ae4e32c785e
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-12 13:40:55 +0600

    php61

---
+++ php61/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Array Functions Bangla Tutorial Part-14 (array_map)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption
+        
+        {
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+        PHP Array Functions Bangla Tutorial Part-14 (array_map)</h2>
+    </div>
+
+   
+
+    <section class="maincontent">
+    <?php
+    function myfunction($value){
+        return ($value + $value);
+    }
+    $arr = array(1,2,3,4,5);
+
+    $result = array_map("myfunction", $arr);
+
+    print("<pre>");
+    print_r($result);
+    print("</pre>");
+  
+    ?>
+  
+    </section>
+
+
+ <div class="footeroption">
+ <p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+</body>
+</html>
