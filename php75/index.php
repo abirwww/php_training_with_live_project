47d4b6d239205a05d8189c91e38e93e040a49b5b
Mehedi Hasan Abir <abirwww770@gmail.com>
2025-05-14 12:13:10 +0600

    php75

---
+++ php75/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Array Functions Bangla Tutorial Part-28 (array_values)</title>
+    <style>
+        .phpcoding{
+            width: 900px;
+            margin: 0 auto;
+            background: #ddd;
+        }
+        .headeroption, .footeroption
+        
+        {
+            background:#444;
+            color:#fff;
+            text-align: center;
+            padding: 20px;
+        }
+        .headeroption h2, .footeroption h2{margin:0}
+        .maincontent{min-height:400px; padding: 20px}
+    </style>
+</head>
+<body>
+
+<section class="phpcoding">
+    <div class="headeroption">
+        <h2>
+   PHP Array Functions Bangla Tutorial Part-28 (array_values)</h2>
+    </div>
+
+    <section class="maincontent">
+    <?php
+   $color = array(
+    "a"=>"red",
+    "b"=>"green",
+    "c"=>"yellow"
+   );
+   $result = array_values($color);
+    
+   print("<pre>");
+   print_r($result);
+   print("</print>");
+
+    ?>
+  
+    </section>
+
+ <div class="footeroption">
+ <p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+<h2><?php echo "www.trainingwithliveproject.com";?></h2>
+ </div>
+</section>
+
+</body>
+</html>
